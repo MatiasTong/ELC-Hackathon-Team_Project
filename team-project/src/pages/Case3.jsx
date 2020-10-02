26456e12d852fbcd809c28a73cc077d62173860b
Matias Tong <matias.tong@gmail.com>
2020-10-01 22:35:46 -0400

    animated form

---
+++ team-project/src/pages/Case3.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+function Case3(props) {
+    return (
+        <div>
+            <p>"you're not alone, contact, speak with"</p>
+<p>"If something doesn't feel right, you got to be your own advocate.
+     You have to be able to fight for yourself and make yourself heard by a doctor"</p>        
+        </div>
+    )
+}
+
+Case3.propTypes = {
+
+}
+
+export default Case3
+
+
+
