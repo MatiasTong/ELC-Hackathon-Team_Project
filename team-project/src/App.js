4bf5b6ad2b90235c27c1cae08e13b33add44f54b
tomDesktop <tomrach11@gmail.com>
2020-10-05 00:30:40 -0400

    add about us

--- team-project/src/App.js
+++ team-project/src/App.js
@@ -49,53 +49,7 @@ function App() {
                   handleSubmit,
                   isSubmitting,
               }) => (
-                  // location={location} key={location.pathname}
-                  <AnimatePresence>
-                      <Switch>
-                          {/* "exact" is needed in this route, otherwise it would match all paths starting with "/" */}
-                          {/* Route paths go here */}
-                          {console.log(values)}
-                          <Route exact path="/">
-                              <Home />
-                          </Route>
-                          <Route path="/questionnaire-age">
-                              <Age />
-                          </Route>
-                          <Route path="/questionnaire-insurance">
-                              <Insurance />
-                          </Route>
-                          <Route path="/result">
-                              <Result />
-                          </Route>
-                          <Route path="/case1">
-                              <Case1 />
-                          </Route>
 
-                          <Route path="/case2">
-                              <Case2 />
-                          </Route>
-                          <Route path="/case1page2">
-                              <Case1Page2 />
-                          </Route>
-                          <Route path="/case1V2">
-                              <Case1V2 />
-                          </Route>
-                          <Route path="/case1V2Step1">
-                              <Case1V2Step1 />
-                          </Route>
-                          <Route path="/case1V2Step2">
-                              <Case1V2Step2 />
-                          </Route>
-                          <Route path="/case1V2Step3">
-                              <Case1V2Step3 />
-                          </Route>
-                          <Route path="/case1V2Step4">
-                              <Case1V2Step4 />
-                          </Route>
-
-                          <Route path="/case4">
-                              <Case4 />
-                          </Route>
 
 // location={location} key={location.pathname}
      <AnimatePresence>
